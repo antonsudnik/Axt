d1a4ed3bc1c93357783b71fc9cd867f38959a35e
antonsudnik <as@red-guild.com>
2026-01-30 20:28:57 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'lora': ['Lora', 'serif'],
+      },
+      colors: {
+        'deep-blue-black': '#1A232E',
+        'bright-aqua': '#00FFFF',
+        'dark-aqua': '#00CCCC',
+        'light-gray-bg': '#F8F9FA',
+        'aqua-promo-bg': '#E0FFFF',
+      }
+    },
+  },
+  plugins: [],
+}
